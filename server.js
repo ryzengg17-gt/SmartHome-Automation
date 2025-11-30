e985ac8efc2aaa244e4088e4072dbf4d4e918b27
ryzengg17-gt <ryzengg17@gmail.com>
2025-11-30 08:59:31 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.json({ message: "Backend Berjalan Dengan Baik!" });
-});
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log("Server berjalan di port " + PORT);
-});
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+  
+    // Username & password tetap
+    if (username === 'admin' && password === '12345') {
+      return res.json({ success: true });
+    } else {
+      return res.json({ success: false, message: 'Username atau password salah!' });
+    }
+  })
